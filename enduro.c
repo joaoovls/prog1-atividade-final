cef280c54d307737a5237f3220c8b687c6087546
joaoovls <jaovictor825@gmail.com>
2025-08-07 01:17:05 -0300

    UPDATE: edsenho datela

--- enduro.c
+++ enduro.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h> //usar para a função de randomizar número
-#include <time.h> //usar para a função de randomizar número
+#include <time.h>   //usar para a função de randomizar número
 #include <stdbool.h>
 
 #ifdef _WIN32
 #include <windows.h> //funções de sleep e system clear na versão windows
-#include <conio.h> //ler a entrada do teclado para sistema windows
+#include <conio.h>   //ler a entrada do teclado para sistema windows
 #else
-#include <unistd.h> //função usleep e system clear
+#include <unistd.h>  //função usleep e system clear
 #include <ncurses.h> //ler a entrada do teclado para sistemas LINUX
 #endif
 
-//definições para o tamanho da rua e para o máximo de inimigos na tela 
-#define COMPRIMENTO_RUA 100
+// definições para o tamanho da rua e para o máximo de inimigos na tela
 #define LARGURA_RUA 75
-#define CARRO_JOGADOR  'A'
-#define CARRO_INIMIGO  'O' 
+#define ALTURA_RUA 10
 #define MAX_CARRO_INIMIGOS 20
 
-//posições dos carros
-typedef struct {
+// posições dos carros
+typedef struct
+{
     int x;
     int y;
 } Carro;
 
-//variáveis para o sistema de dia e o carro
+// variáveis para o sistema de dia e o carro
 Carro carro_jogador;
 Carro carro_inimigo[MAX_CARRO_INIMIGOS];
 int dia = 1;
@@ -34,30 +33,68 @@ int velocidade_carro = 100;
 bool gameOver = false;
 bool começarJogo = false;
 
-//funções da biblioteca ncurses.h
+// funções da biblioteca ncurses.h
 
+// funções
+void entradaTeclado(char key)
+{
+#ifdef _WIN32
 
-
-//funções
-void entradaTeclado(char key) {
-    #ifdef _WIN32
-
-    #else
-        key = getch();
-        switch (key) {
-            case 'a':
-            case 'A':
-                //movimentação para a esquerda <--
-            case 'd':
-            case 'D':
-                //movimentação para a direita <--
-        }
-    #endif    
+#else
+    key = getch();
+    switch (key)
+    {
+    case 'a':
+    case 'A':
+        // movimentação para a esquerda <--
+    case 'd':
+    case 'D':
+        // movimentação para a direita <--
+    }
+#endif
 }
 
-int main(){
-    initscr();
-    while(!gameOver) {
-        
+void systemClear()
+{
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+void posicaoJogador()
+{
+    carro_jogador.x = LARGURA_RUA / 2;
+    carro_jogador.y = ALTURA_RUA - 2;
+}
+
+//OBS: tem q ajeitar essa porra
+void desenharTela()
+{
+    posicaoJogador();
+    systemClear();
+    for (int i = 0; i < ALTURA_RUA; i++)
+    {
+        printf("|");
+        for (int j = 0; j < LARGURA_RUA; j++)
+        {
+            char pixel = '*';
+            if (j == carro_jogador.x && i == carro_jogador.y)
+            {
+                pixel = 'A';
+            }
+            printf("%c", pixel);
+        }
+        printf("|\n");
     }
+}
+
+int main()
+{
+    initscr();
+    // while(!gameOver) {
+    posicaoJogador();
+    desenharTela();
+    //}
 }
