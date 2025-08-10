9976d84db567d93310ab6f8546231ed94ee0e1fa
joaoovls <jaovictor825@gmail.com>
2025-08-10 00:51:46 -0300

    jogo base completo

--- enduro.c
+++ enduro.c
@@ -19,11 +19,12 @@
 #define CHAR_INIMIGO 'O'
 #define CHAR_VAZIO ' '
 
-// posições dos carros
+// variáveis dos carros
 typedef struct
 {
     int x;
     int y;
+    int velocidade;
     bool active;
 } Carro;
 
@@ -32,26 +33,64 @@ Carro carro_jogador;
 Carro carro_inimigo[MAX_CARRO_INIMIGOS];
 int dia = 1;
 int carros_ultrapassados = 0;
-int meta_ultrapassagem = 30;
+int meta_ultrapassagem = 60;
+int usleep_velocidade = 50000; // pra usar no usleep
+int chance_spawn = 15;
 bool gameOver = false;
 bool começarJogo = false;
 
-// funções da biblioteca ncurses.h
-
-// funções
-void systemClear()
+void desenharTela(int inicio_y, int inicio_x, int HUD_x)
 {
 #ifdef _WIN32
-    system("cls");
+
 #else
-    clear();
+    // printa os limites da rua
+    for (int i = 0; i < ALTURA_RUA; i++)
+    {
+        mvaddch(inicio_y + i, inicio_x, '|');
+        mvaddch(inicio_y + i, inicio_x + LARGURA_RUA + 1, '|');
+    }
+
+    // printa os espaços vazios, o carro do jogador e os carros inimigos
+    for (int i = 0; i < ALTURA_RUA; i++)
+    {
+        for (int j = 0; j < LARGURA_RUA; j++)
+        {
+            attron(A_BOLD);
+            mvaddch(ALTURA_RUA, inicio_x + j + 1, '=');
+            attroff(A_BOLD);
+            char caractere_carro = CHAR_VAZIO;
+
+            for (int k = 0; k < MAX_CARRO_INIMIGOS; k++)
+            {
+                if (carro_inimigo[k].active == true && j == carro_inimigo[k].x && i == carro_inimigo[k].y)
+                {
+                    caractere_carro = CHAR_INIMIGO;
+                    break;
+                }
+            }
+
+            if (carro_jogador.x == j && carro_jogador.y == i)
+            {
+                caractere_carro = CHAR_JOGADOR;
+            }
+
+            mvaddch(inicio_y + i, inicio_x + 1 + j, caractere_carro);
+        }
+    }
+
+    carro_jogador.velocidade = usleep_velocidade / 1000;
+    mvprintw(ALTURA_RUA / 2 + 2, HUD_x - 2, "Dias: %d", dia);
+    mvprintw(ALTURA_RUA / 2 + 3, HUD_x - 2, "Ultrapassagens restantes: %d", meta_ultrapassagem);
+    mvprintw(ALTURA_RUA / 2 + 4, HUD_x - 2, "Velocidade: %dkm/h", carro_jogador.velocidade);
+
 #endif
 }
 
-//movimentação dos carros inimigos
-void atualizarInimigos()
+// movimentação dos carros inimigos
+void logicaInimigos()
 {
-    if (rand() % 100 < 15)
+    if (rand() % 100 < chance_spawn)
     {
         for (int i = 0; i < MAX_CARRO_INIMIGOS; i++)
         {
@@ -75,6 +114,15 @@ void atualizarInimigos()
                 carro_inimigo[i].active = false;
             }
         }
+        if (carro_inimigo[i].active == true && carro_inimigo[i].y > carro_jogador.y + 2)
+        {
+            meta_ultrapassagem--;
+        }
+        else if (carro_inimigo[i].y == carro_jogador.y && carro_inimigo[i].x == carro_jogador.x)
+        {
+            gameOver = true;
+            break;
+        }
     }
 }
 
@@ -84,51 +132,37 @@ void posicaoJogador()
     carro_jogador.y = ALTURA_RUA - 2;
 }
 
-void desenharTela(int inicio_y, int inicio_x)
+// lógica de pontuação e sistema do jogo
+void pontuacao()
 {
-#ifdef _WIN32
-
-#else
-    // printa os limites da rua
-    for (int i = 0; i < ALTURA_RUA; i++)
+    if (meta_ultrapassagem <= 0)
     {
-        mvaddch(inicio_y + i, inicio_x, '|');
-        mvaddch(inicio_y + i, inicio_x + LARGURA_RUA + 1, '|');
-    }
+        dia++;
+        chance_spawn += 5;
 
-    // printa os espaços vazios, o carro do jogador e os carros inimigos
-    for (int i = 0; i < ALTURA_RUA; i++)
-    {
-        for (int j = 0; j < LARGURA_RUA; j++)
+        if (chance_spawn > 80)
         {
-            char caractere_carro = CHAR_VAZIO;
-
-            for (int k = 0; k < MAX_CARRO_INIMIGOS; k++)
-            {
-                if (carro_inimigo[k].active == true && j == carro_inimigo[k].x && i == carro_inimigo[k].y)
-                {
-                    caractere_carro = CHAR_INIMIGO;
-                    break;
-                }
-            }
-
-            if (carro_jogador.x == j && carro_jogador.y == i)
-            {
-                caractere_carro = CHAR_JOGADOR;
-            }
-
-            mvaddch(inicio_y + i, inicio_x + 1 + j, caractere_carro);
+            chance_spawn = 80;
         }
 
-        mvprintw(ALTURA_RUA - 1, inicio_x + 1, "Dias: %d", dia);
+        meta_ultrapassagem = 45 + (dia * 15);
     }
+}
+
+// limpar o sistema
+void systemClear()
+{
+#ifdef _WIN32
+    system("cls");
+#else
+    clear();
 #endif
 }
 
 int main()
 {
+    // mudanças no terminal
     srand(time(NULL));
-    // inicialização do ncurses, não aparecer a tecla no terminal, desativar o cursor do terminal e apertar a tecla sem precisar digitar ENTER
     initscr();
     noecho();
     curs_set(0);
@@ -142,21 +176,23 @@ int main()
     int inicio_y = (terminal_y - ALTURA_RUA) / 2;
     int inicio_x = (terminal_x - LARGURA_RUA) / 2;
 
-    posicaoJogador();
+    int HUD_x = (terminal_x / 3 - 9);
 
     for (int i = 0; i < MAX_CARRO_INIMIGOS; i++)
     {
         carro_inimigo[i].active = false;
     }
 
+    posicaoJogador();
+
     // controles do jogador e desenho na tela
     char key;
     while ((key = getch()) != 'q')
     {
 
-        if (gameOver == true) {
-            break;
-        }
+        // if (gameOver == true) {
+        //     break;
+        // }
 
 #ifdef _WIN32
 
@@ -180,24 +216,14 @@ int main()
             break;
         }
 
-        atualizarInimigos();
-
-        //detecta se algum carro inimigo colidiu com o jogador
-        for (int i = 0; i < MAX_CARRO_INIMIGOS; i++)
-    {
-        if (carro_inimigo[i].y == carro_jogador.y && carro_inimigo[i].x == carro_jogador.x)
-        {
-            gameOver = true;
-            break;
-        }
-    }
+        logicaInimigos();
 
         // atualização da tela
         systemClear();
-        desenharTela(inicio_y, inicio_x);
+        desenharTela(inicio_y, inicio_x, HUD_x);
+        pontuacao();
         refresh();
-
-        usleep(50000); //controlar os quadros por segundo
+        usleep(usleep_velocidade / 4); // controlar os quadros por segundo
 #endif
     }
 
